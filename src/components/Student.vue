00b4767661a877825a16647803a701b2be3d8b69
kaola_w <571574523@qq.com>
2023-04-25 12:47:35 +0800

    test_commit

---
+++ src/components/Student.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 结构 -->
+  <div class="demo">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    <button @click="showName">点我提示学生名</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码
+export default {
+  name: 'StudentName',
+  data() {
+    return {
+      name: '张三',
+      age: '18',
+    }
+  },
+  methods: {
+    showName() {
+      alert(this.name)
+    },
+  },
+}
+</script>
+
+<style>
+/* 样式 */
+.demo {
+  background-color: orange;
+}
+</style>
